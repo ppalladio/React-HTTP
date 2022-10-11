75227e40f617a5b7feab473fd96465d42a929d32
Yuxuan <nine.slide@gmail.com>
2022-10-11 17:07:35 +0200

    use async await

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React,{useState}from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movie, setMovie] = useState([])
-    const fetchMoviesHandler = () => {
-        fetch('https://swapi.dev/api/films/')
-            .then((res) => {return res.json()})
-            .then((data) =>{
-              const transMovies = data.results.map(movie =>{
-                return {
-                  id: movie.episode_id,
-                  title: movie.title,
-                  openingText:movie.opening_crawl,
-                  releaseDate:movie.release_date
-                }
-              })
-              setMovie(transMovies)});
+  const [movie, setMovie] = useState([]);
+  async function fetchMoviesHandler() {
+    const res = await fetch('https://swapi.dev/api/films/');
+    const data = await res.json();
+        const transMovies = data.results.map((movie) => {
+            return {
+                id: movie.episode_id,
+                title: movie.title,
+                openingText: movie.opening_crawl,
+                releaseDate: movie.release_date,
+            };
+        });
+        setMovie(transMovies);
     };
-  
+
     return (
         <React.Fragment>
             <section>
@@ -30,6 +29,9 @@ function App() {
             </section>
         </React.Fragment>
     );
-}
+    }
 
 export default App;
+
+    
+   
