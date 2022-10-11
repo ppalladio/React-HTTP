bf14a9b48409a20990456a2518f98756f85edb12
Yuxuan <nine.slide@gmail.com>
2022-10-11 17:14:45 +0200

    handling loading and data

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movie, setMovie] = useState([]);
-  async function fetchMoviesHandler() {
-    const res = await fetch('https://swapi.dev/api/films/');
-    const data = await res.json();
+    const [movie, setMovie] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    async function fetchMoviesHandler() {
+        setIsLoading(true);
+        const res = await fetch('https://swapi.dev/api/films/');
+        const data = await res.json();
         const transMovies = data.results.map((movie) => {
             return {
                 id: movie.episode_id,
@@ -17,21 +19,25 @@ function App() {
             };
         });
         setMovie(transMovies);
-    };
+        setIsLoading(false);
+    }
 
     return (
-        <React.Fragment>
+        <>
             <section>
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
             <section>
-                <MoviesList movies={movie} />
+                {!isLoading && MoviesList.length > 0 && (
+                    <MoviesList movies={movie} />
+                )}
+                {!isLoading && MoviesList.length === 0 && (
+                    <p>No movies found</p>
+                )}
+                {isLoading && <p>Loading</p>}
             </section>
-        </React.Fragment>
+        </>
     );
-    }
+}
 
 export default App;
-
-    
-   
